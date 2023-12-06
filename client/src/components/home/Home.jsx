5b8f0852c8c116dd4fcbb7ee139c48b72f5ed2ea
eLiminatedd <126146757+eLiminatedd@users.noreply.github.com>
2023-12-06 18:18:14 +0200

    Dev (#2)
    * clean up react project
    
    * adding first components

---
+++ client/src/components/home/Home.jsx
@@ -0,0 +1,28 @@
+export default function Home() {
+  return (
+    <>
+      <div className="hero">
+        <article>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            quae dolor natus officia facere. Maiores magnam quos necessitatibus
+            earum exercitationem libero, facilis, consectetur reiciendis quaerat
+            suscipit quibusdam voluptas iste assumenda.
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            quae dolor natus officia facere. Maiores magnam quos necessitatibus
+            earum exercitationem libero, facilis, consectetur reiciendis quaerat
+            suscipit quibusdam voluptas iste assumenda.
+          </p>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            quae dolor natus officia facere. Maiores magnam quos necessitatibus
+            earum exercitationem libero, facilis, consectetur reiciendis quaerat
+            suscipit quibusdam voluptas iste assumenda.
+          </p>
+        </article>
+      </div>
+    </>
+  );
+}
